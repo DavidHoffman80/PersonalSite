dce0eb06a594d1db4efd080c27a3826449409936
Dave Hoffman <spaghettidave80@gmail.com>
2019-08-02 06:47:38 -0400

    Decided not to use Material UI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+// COMPONENTS
+import NavBar from './components/NavBar';
+// CSS
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      
+      <NavBar />
     )
   }
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+
+const NavBar = () => {
+  return (
+    <div className='nav'>
+      <AppBar position='static'>
+        <Toolbar>
+          
+        </Toolbar>
+      </AppBar>
+    </div>
+  )
+}
+
+export default NavBar;
