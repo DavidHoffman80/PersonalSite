4038f0f3fb6390e1f5dbaafac1258ce59c04261a
Dave Hoffman <spaghettidave80@gmail.com>
2019-08-16 16:05:57 -0400

    Added firebase, redux and react-redux

---
+++ src/config/FBconfig.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyBMcuhJi0xNL02ErvGQkfuYJTt1sg0kuiM",
+  authDomain: "personalsite-8baaf.firebaseapp.com",
+  databaseURL: "https://personalsite-8baaf.firebaseio.com",
+  projectId: "personalsite-8baaf",
+  storageBucket: "",
+  messagingSenderId: "663159526375",
+  appId: "1:663159526375:web:efa2a883c7d6cf63"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
